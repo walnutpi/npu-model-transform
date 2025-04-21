9d976678903305ca323ea7482993851cd98914a9
sc-bin <3335447573@qq.com>
2025-04-21 12:00:46 +0800

    format

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ PATH_PWD="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 creat_lnk() {
     link_path=$1
     link_file_name=$2
-    if [  -L $link_path ]; then
+    if [ -L $link_path ]; then
         rm ${link_path}
     fi
     ln -s ${PATH_PWD}/$link_file_name ${link_path}
 }
 
-creat_lnk /usr/bin/npu-model-export  tools/npu-model-export.sh 
-creat_lnk /usr/bin/npu-model-generate  tools/npu-model-generate.sh 
-creat_lnk /usr/bin/npu-tool  tools/npu-tool.sh
+creat_lnk /usr/bin/npu-model-export tools/npu-model-export.sh
+creat_lnk /usr/bin/npu-model-generate tools/npu-model-generate.sh
+creat_lnk /usr/bin/npu-tool tools/npu-tool.sh
--- tools/common.sh
+++ tools/common.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 DOCKER_IMAGE_NAME="ubuntu-npu"
 DOCKER_IMAGE_VER="v1.8.11"
 NPU_VERSION="VIP9000NANOSI_PLUS_PID0X10000016"
@@ -13,13 +12,12 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-
 if docker images --format "{{.Repository}}:{{.Tag}}" | grep -q "^${DOCKER_IMAGE_NAME}$"; then
     echo "docker 镜像 '$DOCKER_IMAGE_NAME' 不存在"
     exit 1
 fi
 
-docker_add_mount(){
+docker_add_mount() {
     # 参数1是想要在docker运行时映射的路径
     local path_to_mount=$1
     if [ ! -d "$path_to_mount" ]; then
@@ -33,39 +31,39 @@ docker_run_bash() {
     local command=$@
     echo "command=$command"
     docker run --name "$CONTAINER_NAME" \
-    --network host \
-    ${PATH_MOUNT} \
-    -v /etc/hosts:/etc/hosts:ro \
-    -v /etc/resolv.conf:/etc/resolv.conf:ro \
-    -v /etc/localtime:/etc/localtime:ro \
-    -v "$(pwd):$(pwd)" \
-    -w "$(pwd)" \
-    -it --rm "$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VER" /bin/bash -c "$command"
+        --network host \
+        ${PATH_MOUNT} \
+        -v /etc/hosts:/etc/hosts:ro \
+        -v /etc/resolv.conf:/etc/resolv.conf:ro \
+        -v /etc/localtime:/etc/localtime:ro \
+        -v "$(pwd):$(pwd)" \
+        -w "$(pwd)" \
+        -it --rm "$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VER" /bin/bash -c "$command"
 }
 
-docker_enter(){
+docker_enter() {
     docker run --name "$CONTAINER_NAME" \
-    --network host \
-    ${PATH_MOUNT} \
-    -v /etc/hosts:/etc/hosts:ro \
-    -v /etc/resolv.conf:/etc/resolv.conf:ro \
-    -v /etc/localtime:/etc/localtime:ro \
-    -v "$(pwd):$(pwd)" \
-    -w "$(pwd)" \
-    -it --rm "$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VER" /bin/bash 
+        --network host \
+        ${PATH_MOUNT} \
+        -v /etc/hosts:/etc/hosts:ro \
+        -v /etc/resolv.conf:/etc/resolv.conf:ro \
+        -v /etc/localtime:/etc/localtime:ro \
+        -v "$(pwd):$(pwd)" \
+        -w "$(pwd)" \
+        -it --rm "$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VER" /bin/bash
 }
 
 docker_run_python3() {
     local command=$@
     docker run --name "$CONTAINER_NAME" \
-    --network host \
-    ${PATH_MOUNT} \
-    -v /etc/hosts:/etc/hosts:ro \
-    -v /etc/resolv.conf:/etc/resolv.conf:ro \
-    -v /etc/localtime:/etc/localtime:ro \
-    -v "$(pwd):$(pwd)" \
-    -w "$(pwd)" \
-    -it --rm "$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VER" /bin/python3 -c "$command"
+        --network host \
+        ${PATH_MOUNT} \
+        -v /etc/hosts:/etc/hosts:ro \
+        -v /etc/resolv.conf:/etc/resolv.conf:ro \
+        -v /etc/localtime:/etc/localtime:ro \
+        -v "$(pwd):$(pwd)" \
+        -w "$(pwd)" \
+        -it --rm "$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VER" /bin/python3 -c "$command"
 }
 
 # export ACUITY_PATH=/root/acuity-toolkit-whl-6.21.16/bin
@@ -85,7 +83,7 @@ pegasus() {
     docker_run_bash python3 /root/acuity-toolkit-whl-6.21.16/bin/pegasus.py $@
 }
 
-generate_quantize(){
+generate_quantize() {
     local IMAGE_FILES_PATH=$1
     local TMP_FILE_PREFIX=$2
     local dataset_path="$(dirname $TMP_FILE_PREFIX)/dataset.txt"
@@ -94,11 +92,11 @@ generate_quantize(){
         exit 1
     fi
     docker_add_mount "$(dirname $IMAGE_FILES_PATH)"
-    find ${IMAGE_FILES_PATH}/* > ${dataset_path}
+    find ${IMAGE_FILES_PATH}/* >${dataset_path}
     pegasus quantize --model ${TMP_FILE_PREFIX}.json --model-data ${TMP_FILE_PREFIX}.data --device CPU --with-input-meta ${TMP_FILE_PREFIX}_inputmeta.yml --compute-entropy --rebuild --model-quantize ${TMP_FILE_PREFIX}_uint8.quantize --quantizer asymmetric_affine --qtype uint8
 }
 
-generate_nb_model(){
+generate_nb_model() {
     local TMP_FILE_PREFIX=$1
     local OUTPU_FILENAME=$2
     local VIV_SDK="/root/Vivante_IDE/VivanteIDE5.8.2/cmdtools"
